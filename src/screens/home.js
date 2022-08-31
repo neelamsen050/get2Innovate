2c53f7a3700d49b828fee9ff094854d05a3ef631
neelamsen050 <neelamsen050@gmail.com>
2022-09-01 01:45:40 +0530

    Adding files

---
+++ src/screens/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+export default HomeScreen;
+
